58d8ea121d6efb25bbb32c5f8202ab3245833504
devyourown <hyojoon56@gmail.com>
2024-10-07 18:00:09 +0900

    making ...

--- app/coupons/page.tsx
+++ app/coupons/page.tsx
@@ -1,33 +1,57 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Modal from "../components/orders/upload-modal";
+import Pagination from "../components/pagination";
+import { fetchCouponsByPage, fetchCouponsByUser } from "../lib/api";
+
 export default function CouponList() {
-    // 임시 데이터
-    const coupons = [
-        { id: 1, code: 'DISCOUNT10', discount: 10, validUntil: '2024-12-31' },
-        { id: 2, code: 'FREESHIP', discount: 0, validUntil: '2024-12-31', freeShipping: true },
-    ];
+    const [coupons, setCoupons] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+
+  useEffect(() => {
+  }, []);
+
+  async function fetchOrders(page: number) {
+    const data = await fetchCouponsByPage(page);
+    setCoupons(data.content);
+    setTotalPages(data.page.totalPages);
+  }
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
 
     return (
-        <div className="bg-white p-4 rounded-lg shadow">
-            <h2 className="text-xl font-semibold mb-4">쿠폰 목록</h2>
-            <table className="min-w-full bg-white">
-                <thead>
-                    <tr>
-                        <th className="py-2">쿠폰 ID</th>
-                        <th className="py-2">코드</th>
-                        <th className="py-2">할인율</th>
-                        <th className="py-2">유효기간</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {coupons.map(coupon => (
-                        <tr key={coupon.id}>
-                            <td className="py-2">{coupon.id}</td>
-                            <td className="py-2">{coupon.code}</td>
-                            <td className="py-2">{coupon.discount}%</td>
-                            <td className="py-2">{coupon.validUntil}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">쿠폰 관리</h1>
+          <div className="flex justify-between items-center mb-4">
+            <div>
+              <button className="bg-white border border-gray-300 text-gray-700 px-4 py-2 rounded mr-2"
+              onClick={openModal}>
+                쿠폰 만들기
+              </button>
+              <button className="bg-white border border-gray-300 text-gray-700 px-4 py-2 rounded mr-2">
+                쿠폰 삭제하기
+              </button>
+            </div>
+          </div>
+          <Pagination
+            currentPage={currentPage} 
+            totalPages={totalPages} 
+            onPageChange={(page) => setCurrentPage(page)} 
+          />
+          {isModalOpen && (
+            <Modal onClose={closeModal}>
+              <h2 className="text-lg font-semibold mb-4">쿠폰 만들기</h2>
+            </Modal>
+          )}
         </div>
-    );
+      );
 }
